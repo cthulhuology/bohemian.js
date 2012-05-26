76271dad4c29748011d2d1b1b60845dc7aa55b2a
Dave Goehrig <dave@nexttolast.com>
2012-05-26 14:18:13 -0300

    testing...

--- bohemian.js
+++ bohemian.js
@@ -2,8 +2,8 @@
 
 _ = Bohemian = function() { 
 	var self = arguments.callee
-	self.history =  self.history ? self.history : []
-	self.history.push(Array.prototype.splice.apply(arguments,[0]))
+//	self.history =  self.history ? self.history : []
+//	self.history.push(Array.prototype.splice.apply(arguments,[0]))
 	if (typeof(self[arguments[0]]) == 'function') 
 		self._ = self[arguments[0]].apply(self,Array.prototype.splice.apply(arguments,[1]))
 	else if (typeof(self._[arguments[0]]) == 'function')
