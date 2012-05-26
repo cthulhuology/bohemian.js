b1016242f3c393da028f12b591879e6b94fe78f7
Dave Goehrig <dave@nexttolast.com>
2012-05-26 15:48:34 -0300

    Adding clearing the canvas

--- bohemian.js
+++ bohemian.js
@@ -168,6 +168,8 @@ _('method','does',function() {
 	},
 	'clear', function() {
 		while(document.body.firstChild) document.body.removeChild(document.body.firstChild)
+		this.context().clearRect(0,0,this.screen().width,this.screen().height)
+		document.body.add(this.screen())
 		return this._
 	},
 	'context', function() {
